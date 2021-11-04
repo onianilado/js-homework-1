252d0d980c8d5dac2c4fb7527ad8981f80402b94
onianilado <oniani4@yahoo.com>
2021-11-04 17:43:53 +0400

    modified: script js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let arr = ['javascript', 'php', 'c#', 'python', 'c++', 'kotlin', 'c', 'swift'];
 for (let i = 0; i < arr.length; i++){
-    if(arr[i].length >= 5){
-        console.log(arr[i])
+    if(arr[i].length < 5){
+        continue
     }
     else{ 
-        continue
+        console.log(arr[i])
     }
 }
