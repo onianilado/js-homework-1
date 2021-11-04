94ec3b7e90e965202b357f38db001363247f8183
Lado <86075633+onianilado@users.noreply.github.com>
2021-11-04 18:02:59 +0400

    modified: script js

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ for (let i = 0; i < arr.length; i++){
         console.log(arr[i])
     }
 }
+// let arr1 = [2, -3, 5, 11, 1, -30, -11, 100, -1, -3, -4];
+// for (let i = 0; i< arr1.length; i++){
+//     if(arr1[i] > 0){
+//         console.log(arr1[i])
+//     }
+// }
+// let arr2 = [[2, -3, 5, 11], [1, -30, -11, 100], [-1, -3, -4]];
+// for (let i = 0; i < arr2.length; i++){
+//     for (let j = 0; j < arr2[i].length; j++){
+//         if(arr2[i][j] > 0){
+//             console.log(arr2[i][j])
+//         }
+//     }
+// }
